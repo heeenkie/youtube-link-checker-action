5b21f4253d22f5412b12f0fa9a66bf0ede2584e3
heeenkie <35926672+heeenkie@users.noreply.github.com>
2021-03-27 17:50:16 +0100

    update

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { promises: fs, symlinkSync } = require('fs')
 try {
   // `who-to-greet` input defined in action metadata file
   var filePaths = core.getInput('file-path');
+  console.log("Test-- " + filePaths)
   filePaths.replace('[', '')
   filePaths.replace(']', '')
   filePaths.replace('"', '')
