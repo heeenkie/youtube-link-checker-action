f04c616cc1d7883e1b552379792f4935329682c8
heeenkie <35926672+heeenkie@users.noreply.github.com>
2021-03-27 14:23:13 +0100

    updates

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const github = require('@actions/github');
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
+  const nameToGreet = core.getInput('action-name');
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  core.setOutput("reason", "Video meets the requirements");
+  core.setOutput("reason", "Video does not meet the requirements");
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  //console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
