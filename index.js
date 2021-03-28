c5d10c926e435ea36ed0d49193e0ab4a3e0ab168
heeenkie <35926672+heeenkie@users.noreply.github.com>
2021-03-28 10:24:50 +0200

    update

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ try {
     if (path.toLowerCase().includes('readme')) {
       console.log('Readme file detected');
       fs.readFile(path, 'utf8').then((content) => {
-        //const matches = findAllURLs(content);
         var videoId = youtube_videoId_parser(content);
         console.log(videoId);
       }).catch(error => core.setFailed(error.message));
@@ -23,13 +22,18 @@ try {
 function youtube_videoId_parser(url) {
     var regExp = /.*(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*).*/;
     var match = url.match(regExp);
-    for (var m of match) {
-      console.log('loop- ' + m);
-    }
-    console.log('length: ' + match[1].trim().length);
     return (match&&match[1].trim().length==11)? match[1].trim() : false;
 }
 
 function findAllURLs(str) {
   return str.match(/\bhttp?::\/\/\S+/gi);
 }
+
+
+// http://www.youtube.com/watch?v=0zM3nApSvMg&feature=feedrec_grec_index
+// http://www.youtube.com/user/IngridMichaelsonVEVO#p/a/u/1/QdK8U-VIH_o
+// http://www.youtube.com/v/0zM3nApSvMg?fs=1&amp;hl=en_US&amp;rel=0
+// http://www.youtube.com/watch?v=0zM3nApSvMg#t=0m10s
+// http://www.youtube.com/embed/0zM3nApSvMg?rel=0
+// http://www.youtube.com/watch?v=0zM3nApSvMg
+// http://youtu.be/0zM3nApSvMg
