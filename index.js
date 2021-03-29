f6a7ca81afdbc52bfab52941647b0461be3f0d92
heeenkie <35926672+heeenkie@users.noreply.github.com>
2021-03-29 11:02:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -36,15 +36,6 @@ function findAllURLs(str) {
   return str.match(/(https?\:\/\/)?([^\.\s]+)?[^\.\s]+\.[^\s]+/gi);
 }
 
-// http://www.youtube.com/watch?v=0zM3nApSvMg&feature=feedrec_grec_index
-// http://www.youtube.com/user/IngridMichaelsonVEVO#p/a/u/1/QdK8U-VIH_o
-// http://www.youtube.com/v/0zM3nApSvMg?fs=1&amp;hl=en_US&amp;rel=0
-// http://www.youtube.com/watch?v=0zM3nApSvMg#t=0m10s
-// http://www.youtube.com/embed/0zM3nApSvMg?rel=0
-// http://www.youtube.com/watch?v=0zM3nApSvMg
-// http://youtu.be/0zM3nApSvMg
-// Wrapped in paranteses
-
 function checkDuration(id) {
   let key = core.getInput('youtube-api-key');
   let uRl = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${id}&key=${key}`;
